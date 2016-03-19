074d2713f8f4e42253a0589b56a7d6754493d01d
Guillaume BILLEY <guillaume.billey@pharmagest.com>
2016-03-19 17:17:26 +0100

    [FEAT](PlanB) : Mock mock mock

--- src/main/webapp/scripts/app/app.js
+++ src/main/webapp/scripts/app/app.js
@@ -1,9 +1,9 @@
 'use strict';
 
-angular.module('hackinghealthApp', ['LocalStorageModule', 'tmh.dynamicLocale', 'pascalprecht.translate', 
-    'ngResource', 'ngCookies', 'ngAria', 'ngCacheBuster', 'ngFileUpload',
-    // jhipster-needle-angularjs-add-module JHipster will add new module here
-    'ui.bootstrap', 'ui.router',  'infinite-scroll', 'angular-loading-bar'])
+angular.module('hackinghealthApp', ['LocalStorageModule', 'tmh.dynamicLocale', 'pascalprecht.translate',
+        'ngResource', 'ngCookies', 'ngAria', 'ngCacheBuster', 'ngFileUpload',
+        // jhipster-needle-angularjs-add-module JHipster will add new module here
+        'ui.bootstrap', 'ui.router', 'infinite-scroll', 'angular-loading-bar'])
 
     .run(function ($rootScope, $location, $window, $http, $state, $translate, Language, Auth, Principal, ENV, VERSION) {
         // update the window title using params in the following
@@ -11,7 +11,7 @@ angular.module('hackinghealthApp', ['LocalStorageModule', 'tmh.dynamicLocale', '
         // 1. titleKey parameter
         // 2. $state.$current.data.pageTitle (current state page title)
         // 3. 'global.title'
-        var updateTitle = function(titleKey) {
+        var updateTitle = function (titleKey) {
             if (!titleKey && $state.$current.data && $state.$current.data.pageTitle) {
                 titleKey = $state.$current.data.pageTitle;
             }
@@ -19,7 +19,7 @@ angular.module('hackinghealthApp', ['LocalStorageModule', 'tmh.dynamicLocale', '
                 $window.document.title = title;
             });
         };
-        
+
         $rootScope.ENV = ENV;
         $rootScope.VERSION = VERSION;
         $rootScope.$on('$stateChangeStart', function (event, toState, toStateParams) {
@@ -29,25 +29,25 @@ angular.module('hackinghealthApp', ['LocalStorageModule', 'tmh.dynamicLocale', '
             if (Principal.isIdentityResolved()) {
                 Auth.authorize();
             }
-			
-            
+
+
             // Update the language
             Language.getCurrent().then(function (language) {
                 $translate.use(language);
             });
-            
+
         });
 
-        $rootScope.$on('$stateChangeSuccess',  function(event, toState, toParams, fromState, fromParams) {
-            var titleKey = 'global.title' ;
+        $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+            var titleKey = 'global.title';
 
             // Remember previous state unless we've been redirected to login or we've just
             // reset the state memory after logout. If we're redirected to login, our
             // previousState is already set in the authExpiredInterceptor. If we're going
             // to login directly, we don't want to be sent to some previous state anyway
             if (toState.name != 'login' && $rootScope.previousStateName) {
-              $rootScope.previousStateName = fromState.name;
-              $rootScope.previousStateParams = fromParams;
+                $rootScope.previousStateName = fromState.name;
+                $rootScope.previousStateParams = fromParams;
             }
 
             // Set the page title key to the one configured in state or use default one
@@ -56,12 +56,14 @@ angular.module('hackinghealthApp', ['LocalStorageModule', 'tmh.dynamicLocale', '
             }
             updateTitle(titleKey);
         });
-        
-        // if the current translation changes, update the window title
-        $rootScope.$on('$translateChangeSuccess', function() { updateTitle(); });
 
-        
-        $rootScope.back = function() {
+        // if the current translation changes, update the window title
+        $rootScope.$on('$translateChangeSuccess', function () {
+            updateTitle();
+        });
+
+
+        $rootScope.back = function () {
             // If previous state is 'activate' or do not exist go to 'home'
             if ($rootScope.previousStateName === 'activate' || $state.get($rootScope.previousStateName) === null) {
                 $state.go('home');
@@ -83,30 +85,50 @@ angular.module('hackinghealthApp', ['LocalStorageModule', 'tmh.dynamicLocale', '
 
         $urlRouterProvider.otherwise('/');
         $stateProvider.state('site', {
-            'abstract': true,
-            views: {
-                'navbar@': {
-                    templateUrl: 'scripts/components/navbar/navbar.html',
-                    controller: 'NavbarController'
-                }
-            },
-            resolve: {
-                authorize: ['Auth',
-                    function (Auth) {
-                        return Auth.authorize();
+                'abstract': true,
+                views: {
+                    'navbar@': {
+                        templateUrl: 'scripts/components/navbar/navbar.html',
+                        controller: 'NavbarController'
                     }
-                ],
-                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
-                    $translatePartialLoader.addPart('global');
-                }]
-            }
-        });
+                },
+                resolve: {
+                    authorize: ['Auth',
+                        function (Auth) {
+                            return Auth.authorize();
+                        }
+                    ],
+                    translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                        $translatePartialLoader.addPart('global');
+                    }]
+                }
+            })
+            .state('stockAction', {
+                parent: 'entity',
+                url: '/stockActions',
+                data: {
+                    pageTitle: 'hackinghealthApp.stock.home.title',
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'scripts/app/entities/stock/stock-action.html',
+                        controller: 'StockController'
+                    }
+                },
+                resolve: {
+                    translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                        $translatePartialLoader.addPart('stock');
+                        $translatePartialLoader.addPart('global');
+                        return $translate.refresh();
+                    }]
+                }
+            });
 
         $httpProvider.interceptors.push('errorHandlerInterceptor');
         $httpProvider.interceptors.push('authExpiredInterceptor');
         $httpProvider.interceptors.push('notificationInterceptor');
         // jhipster-needle-angularjs-add-interceptor JHipster will add new application interceptor here
-        
+
         // Initialize angular-translate
         $translateProvider.useLoader('$translatePartialLoader', {
             urlTemplate: 'i18n/{lang}/{part}.json'
@@ -120,16 +142,24 @@ angular.module('hackinghealthApp', ['LocalStorageModule', 'tmh.dynamicLocale', '
         tmhDynamicLocaleProvider.localeLocationPattern('bower_components/angular-i18n/angular-locale_{{locale}}.js');
         tmhDynamicLocaleProvider.useCookieStorage();
         tmhDynamicLocaleProvider.storageKey('NG_TRANSLATE_LANG_KEY');
-        
+
     })
     // jhipster-needle-angularjs-add-config JHipster will add new application configuration here
-    .config(['$urlMatcherFactoryProvider', function($urlMatcherFactory) {
+    .config(['$urlMatcherFactoryProvider', function ($urlMatcherFactory) {
         $urlMatcherFactory.type('boolean', {
-            name : 'boolean',
-            decode: function(val) { return val == true ? true : val == "true" ? true : false },
-            encode: function(val) { return val ? 1 : 0; },
-            equals: function(a, b) { return this.is(a) && a === b; },
-            is: function(val) { return [true,false,0,1].indexOf(val) >= 0 },
+            name: 'boolean',
+            decode: function (val) {
+                return val == true ? true : val == "true" ? true : false
+            },
+            encode: function (val) {
+                return val ? 1 : 0;
+            },
+            equals: function (a, b) {
+                return this.is(a) && a === b;
+            },
+            is: function (val) {
+                return [true, false, 0, 1].indexOf(val) >= 0
+            },
             pattern: /bool|true|0|1/
         });
     }]);
--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -1,13 +1,21 @@
 'use strict';
 
 angular.module('hackinghealthApp')
-    .controller('MainController', function ($scope, Principal) {
+    .controller('MainController', function ($scope, $state, Principal, Medicine) {
         Principal.identity().then(function(account) {
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
             
         });
         $scope.scanCIP = function(){
-            alert($scope.cip);
+            //Medicine.
+            $state.go('stockAction',
+                {
+                    medicine: {
+                        name:"DAFALGAN 500mg CPR",
+                        cip:$scope.cip,
+                        monographie:'une monographie'
+                    }
+                });
         }
     });
